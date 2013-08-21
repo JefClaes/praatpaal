29e4598101cfd8ab0903faa7220ece926de3340c
Jef Claes <jef.claes@gmail.com>
2013-08-21 21:40:23 +0200

    Added some content

--- src/PraatPaal/PraatPaal/HomeModule.cs
+++ src/PraatPaal/PraatPaal/HomeModule.cs
@@ -11,6 +11,9 @@ namespace PraatPaal
         public HomeModule()
         {
             Get["/"] = x => { return View["home.cshtml"]; };
+            Get["/onsteam.html"] = x => { return View["onsteam.cshtml"]; };
+            Get["/afspraakmaken.html"] = x => { return View["afspraakmaken.cshtml"]; };
+            Get["/locatie.html"] = x => { return View["locatie.cshtml"]; };
         }
     }
 }

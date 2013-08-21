2b845ed2bfc50581374669f05c8bbf817d0121e8
Jef Claes <jef.claes@gmail.com>
2013-08-21 20:34:45 +0200

    Hello world

---
+++ src/PraatPaal/PraatPaal/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace PraatPaal
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = x => { return "Hello!"; };
+        }
+    }
+}
